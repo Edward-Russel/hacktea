5dfebce52d7b5f151f2060db5a77057afe1462ea
InfinityExistence <wolfgolder@gmail.com>
2022-12-10 17:21:02 +0300

    WifiScanArrived

--- app/App.js
+++ app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, TouchableOpacity, SafeAreaView, Button, PermissionsAndroid } from 'react-native';
 import styles from './style'
-
+import WifiBlob from './wifi'
 /**
  * Посылает пользователю push-уведомление
  *
@@ -12,6 +12,35 @@ function userSendNotification(text) {
   alert("Типо уведомление")
 }
 
+/**
+ * Запрос разрешения для Wifi сканера 
+ * PERMISSIONS.ACCESS_FINE_LOCATION
+ * Ну и отладка в консоль
+ * */
+const requestFineLocation = async () => {
+    try {
+const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    {
+        title: 'Location permission is required for WiFi connections',
+        message:
+            'This app needs location permission as this is required  ' +
+            'to scan for wifi networks.',
+        buttonNegative: 'DENY',
+        buttonPositive: 'ALLOW',
+    },
+);
+if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    // You can now use react-native-wifi-reborn
+    console.log("You can use the wifi scaner");
+} else {
+    // Permission denied
+    console.log("Wifi scaner permission denied");
+        }
+    } catch (err) {
+        console.warn(err);
+    }
+};
 
 const App = () => {
   return (
@@ -23,8 +52,10 @@ const App = () => {
           onPress={userSendNotification}
         >
           <Text style={styles.text}>Привет Гена</Text>
-          </TouchableOpacity>
-      </View>
+              </TouchableOpacity>
+          </View>
+          <Button title="request permissions" onPress={requestFineLocation} />
+          <WifiBlob />
       <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
   </SafeAreaView>
   )
