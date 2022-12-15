1aa12f8332520b20a40195d34952d79286a03dc4
InfinityExistence <wolfgolder@gmail.com>
2022-12-15 06:58:15 +0300

    WifiImprove

--- app/pages/wifi_debug.js
+++ app/pages/wifi_debug.js
@@ -1,7 +1,27 @@
-import WifiBlob from '../wifi'
-import { View, Text, TouchableOpacity, SafeAreaView, Button, PermissionsAndroid, Image } from 'react-native';
+import WifiBlob from '../wifi';
+import { View, Text, TouchableOpacity, SafeAreaView, Button, PermissionsAndroid, Image, TextInput } from 'react-native';
 import styles from '../style'
+import WifiManager from "react-native-wifi-reborn";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
 
+const storeData = async (value) => {
+    try {
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem('@storage_Key', jsonValue);
+    } catch (e) {
+        // saving error
+    }
+}
+
+const addData = (BSSID, Xo, Yo, onemeter, data) => {
+    data.routers[BSSID] = {
+        x: Xo,
+        y: Yo,
+        onemeter: onemeter
+    };
+    return data;
+}
 
 /**
  * Запрос разрешения для Wifi сканера 
@@ -24,6 +44,7 @@ const granted = await PermissionsAndroid.request(
 if (granted === PermissionsAndroid.RESULTS.GRANTED) {
     // You can now use react-native-wifi-reborn
     console.log("You can use the wifi scaner");
+    WifiManager.setEnabled(true);
 } else {
     // Permission denied
     console.log("Wifi scaner permission denied");
@@ -34,6 +55,84 @@ if (granted === PermissionsAndroid.RESULTS.GRANTED) {
 };
 
 module.exports = () => {
+    const [coord, setCoord] = useState([0,0]);
+    const [Xo, setXo] = useState(0);
+    const [Yo, setYo] = useState(0);
+    const [BSSID, setBSSID] = useState("");
+    const [onemeter, setOnemeter] = useState(0);
+    const [data, setdate] = useState({
+        id: 24351,
+        routers: {
+            '4a:5a:b6:dc:65:9f': {
+                x: 7,
+                y: 7,
+                onemeter: -41
+
+            },
+            'a1:23:aa:12:2a': {
+                x: 3,
+                y: 5,
+                onemeter: -45
+
+            },
+            'a4:23:22:12:1a': {
+                x: 6,
+                y: 3,
+                onemeter: -40
+
+            },
+            'c4:6e:1f:0d:21:a6': {
+                x: 10,
+                y: 10,
+                onemeter: -39
+
+            },
+            '78:02:f8:2c:d8:1e': {
+                x: 1,
+                y: 10,
+                onemeter: -38
+
+            },
+            'ce:07:e4:f2:96:3f': {
+                x: 10,
+                y: 1,
+                onemeter: -32
+
+            },
+        }
+    });
+    const [list, setList] = useState(0);
+
+
+
+
+
+
+
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            
+            WifiBlob.getCoordinates().then((crd) => {
+                setCoord(crd);
+            });
+            WifiBlob.getList().then((crd) => {
+                setList(crd);
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+
+    }, []); 
+    
+    
+    const load = () => {
+       
+        WifiBlob.getdate().then((crd) => {
+            setdate(crd);
+        });
+
+    };
     return (
         <SafeAreaView style={{flex: 1}}>
         <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
@@ -45,8 +144,49 @@ module.exports = () => {
                 <Text style={styles.text}>Где я?</Text>
                     </TouchableOpacity>
                 </View>
-                <Button title="request permissions" onPress={requestFineLocation} />
-            <WifiBlob />
+            <Button title="request permissions" onPress={requestFineLocation} />
+
+
+            <View>
+                <Button onPress={() => {load()}} title="reload" />
+                <Text>Coords1:{coord[0]} {coord[1]} </Text>
+                <Button onPress={() => { storeData(data) }} title="data push" />
+                <Button onPress={() => { setdate(addData(BSSID, Xo, Yo, onemeter, data)) }} title="data set +" />
+                <TextInput
+                    style={{ height: 35 }}
+                    placeholder="BSSID"
+                    onChangeText={newText => setBSSID(newText)}
+                    defaultValue={BSSID}
+                />
+                <TextInput
+                    keyboardType="numeric"
+                    style={{ height: 35 }}
+                    placeholder="X"
+                    onChangeText={newText => setXo(Number(newText))}
+                    defaultValue={Xo}
+                />
+                <TextInput
+                    keyboardType="numeric"
+                    style={{ height: 35 }}
+                    placeholder="Y"
+                    onChangeText={newText => setYo(Number(newText))}
+                    defaultValue={Yo}
+                />
+                <TextInput
+                    keyboardType="numeric"
+                    style={{ height: 35 }}
+                    placeholder="In one meter P0"
+                    onChangeText={newText => setOnemeter(Number(newText))}
+                    defaultValue={onemeter}
+                />
+
+                <Text style={{ height: 100 }} selectable={true} > {JSON.stringify(list)}  </Text>
+                <Text style={{ height: 100 }} selectable={true} > {JSON.stringify(data)}  </Text>
+            </View>
+            
+
+
+
             <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
         </SafeAreaView>
         )
--- app/wifi.js
+++ app/wifi.js
@@ -1,74 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, Text, View, Button } from 'react-native';
 import WifiManager from "react-native-wifi-reborn";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+let wifilist = null;
+let data;
 
 
-//server.getData(data => ...)
 
-const data = {
-    id: 24351,
-    routers: {
-        '4a:5a:b6:dc:65:9f': {
-            x: 7,
-            y: 7,
-            onemeter: -41
 
-            },
-        'a1:23:aa:12:2a': {
-            x: 3,
-            y: 5,
-            onemeter: -45
 
-        },
-        'a4:23:22:12:1a': {
-            x: 6,
-            y: 3,
-            onemeter: -40
 
-        },
-        'c4:6e:1f:0d:21:a6': {
-            x: 10,
-            y: 10,
-            onemeter: -39
-
-        },
-        '78:02:f8:2c:d8:1e': {
-            x: 1,
-            y: 10,
-            onemeter: -38
-
-        },
-        'ce:07:e4:f2:96:3f': {
-            x: 10,
-            y: 1,
-            onemeter: -32
-
-        },
+const getData = async () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem('@storage_Key');
+        data = JSON.parse(jsonValue);
+        return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+        // error reading value
     }
 }
 
 
-
-/**
- * Считаем время, чтобы определить измменение списка сканирования
- * Избавляет от лишних расчетов
- * */
-function timestamp(output) {
-    let sum = 0;
-    
-
-    for (let i of output) {
-        sum += i["timestamp"];
-    }
-
-    return (sum);
-}
-
-function distance(p0, p1, n) {
+function distance(p0, p1, n, thatdist) {
 
     const newdist = Math.pow(10, (p0 - p1) / (10 * n));
 
-    if ((newdist < 15) || (n === 6)) {
+    if ((newdist < thatdist) || (n === 6)) {
         return newdist;
     }
 
@@ -82,16 +38,15 @@ function distance(p0, p1, n) {
 function triangulation(output) {
     let r1, r2, x1, x2, y1, y2;
     let x, y;
-
     for (let itm of output) {
         if ((data["routers"][itm["BSSID"]] !== undefined) && (r1 === undefined)) {
-            r1 = distance(data["routers"][itm["BSSID"]]["onemeter"], itm["level"], 2);
+            r1 = distance(data["routers"][itm["BSSID"]]["onemeter"], itm["level"], 2, 19);
             x1 = data["routers"][itm["BSSID"]]["x"];
             y1 = data["routers"][itm["BSSID"]]["y"];
             continue;
         }
         if ((data["routers"][itm["BSSID"]] !== undefined)) {
-            r2 = distance(data["routers"][itm["BSSID"]]["onemeter"], itm["level"], 2);
+            r2 = distance(data["routers"][itm["BSSID"]]["onemeter"], itm["level"], 2, 15 + r1*r1);
             x2 = data["routers"][itm["BSSID"]]["x"];
             y2 = data["routers"][itm["BSSID"]]["y"];
             let d = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
@@ -100,26 +55,18 @@ function triangulation(output) {
             let xz = x1 + a * (x2 - x1) / d;
             let yz = y1 + a * (y2 - y1) / d;
             if (x === undefined) {
-                console.log("firstdetect")
                 x = [xz + h * (y2 - y1) / d, xz - h * (y2 - y1) / d];
                 y = [yz - h * (x2 - x1) / d, yz + h * (x2 - x1) / d];
-                console.log(x);
                 continue;
             }  // две координаты ищем ближайшую
-            console.log("other");
             let tmpx = [xz + h * (y2 - y1) / d, xz - h * (y2 - y1) / d];
             let tmpy = [yz - h * (x2 - x1) / d, yz + h * (x2 - x1) / d];
             let mindistx = 15;
             let mindisty = 15;
             let out = [0, 0, 0, 0];
-            console.log(x, r1);
-            console.log(tmpx, r2);
             for (let i in x) {
                 for (let z in tmpx) {
-                    
-                    console.log(i, x[i], x);
                     if ((Math.abs(tmpx[z] - x[i]) < mindistx) && (Math.abs(tmpy[z] - y[i]) < mindisty)) {
-                        console.log("work");
                         mindistx = Math.abs(tmpx[z] - x[i]);
                         mindisty = Math.abs(tmpy[z] - y[i]);
                         out = [tmpx[z], tmpy[z], x[i], y[i]];
@@ -137,49 +84,96 @@ function triangulation(output) {
 }
 
 
+
+
+const WifiBlob = ((exports) => {
+
+    function getCoordinates() {
+        return new Promise((resolve, reject) => {
+            getData().then(() => {
+                WifiManager.loadWifiList()
+                    .then((output) => {
+                        wifilist = output;
+                        resolve(triangulation(output));
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            });
+        });
+    };
+    function getList() {
+        return new Promise((resolve, reject) => {
+            resolve(wifilist);
+            });
+        };
+    function getdate() {
+        return new Promise((resolve, reject) => {
+            resolve(data);
+        });
+    };
+
+    exports.getdate = getdate;
+    exports.getCoordinates = getCoordinates;
+exports.getList = getList;
+    return exports;
+})({});
+
+export default WifiBlob;
+
+/*
+
 const WifiBlob = () => {
-    const [wifilist, setWifilist] = useState([0,0]);
-    const [sumofsignal, setSumOfSignals] = useState(0);
-    const [timedelay, setTimeDelay] = useState(0);
-    const [usercoord, setUserCoord] = useState([0,0]);
-   
+    const [usercoord, setUserCoord] = useState([0, 0]);
+
     useEffect(() => {
+        const onload = setTimeout(() => {
+            getData().then((output) => { window.dater = output; });
+            
+            WifiManager.loadWifiList().then((output) => {
+                
+                setUserCoord(triangulation(output));
+                
+                console.log(window.lister);
+                
+            }).catch((error) => {
+                console.log(error)
+            });
+
+
+            
+           
+            console.log(" I do this once");
+        }, 1);
+
+
         const interval = setInterval(() => {
             WifiManager.reScanAndLoadWifiList().then((output) => {
-                console.log(output);
-                setWifilist([output[0]["level"], output[1]["level"]]);
-
-                const sum = timestamp(output);
-                if (sum !== sumofsignal) {
+                let sum = timestamp(output);
+                if ((sum !== 0) || (sum !== sumofsignal)) {
+                    wifilist = output;
+                    
+                    window.lister = wifilist;
                     setUserCoord(triangulation(output));
-                    console.log(usercoord);
                 }
-
-
-                setSumOfSignals(sum);
-
+                sumofsignal = sum;
 
             }).catch((error) => {
                 console.log(error)
             });
 
-            setTimeDelay(timedelay => timedelay + 1);
+            timedelay += 1;
 
         }, 1000);
+
         return () => clearInterval(interval);
+
     }, []); 
-  
+    
+    return usercoord;
+};  
 
 
-    return (
-        <View>
-            <Text>Time: {timedelay } seconds</Text>
-            <Text>Timestamp: {sumofsignal} </Text>
-            <Text>Coords1: {usercoord[0]} {usercoord[1]}  </Text>
-            <Text>Radius: {wifilist[0]} {wifilist[1]}</Text>
-        </View>
-    );
-}     
 
-
-export default WifiBlob;
+export {
+    storeData, addData, getData}; */
